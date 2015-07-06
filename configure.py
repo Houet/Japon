6fbe476d86746d2e543bade8677429a36ea74d9d
Houet <corentin.houet@gmail.com>
2015-07-06 12:37:32 +0900

    respect pep8

--- configure.py
+++ configure.py
@@ -1,61 +1,65 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-""" fenetre de configuration pour interface principale"""
+""" settings window for SDGI programm (cf interface.py) """
 
 from tkinter import *
 
+
 class Configure(Frame):
-	def __init__(self, window, wth, wtp, wbt, wet, **kwargs):
-		Frame.__init__(self, window, **kwargs)
-		self.pack(fill=BOTH)
-		self.window = window
-		self.rendu = ()
+    def __init__(self, window, wth, wtp, wbt, wet, **kwargs):
+        Frame.__init__(self, window, **kwargs)
+        self.pack(fill=BOTH)
+        self.window = window
+        self.rendu = ()
 
+        Label(self, text="Threshold resolution:").grid(column=1,
+                                                       row=1,
+                                                       sticky=W)
+        self.wth = Scale(self, from_=1, orient=HORIZONTAL, to=20,
+                         resolution=1, length=200)
+        self.wth.grid(column=2, row=1, columnspan=2)
+        self.wth.set(wth)
 
-		Label(self, text="Threshold resolution:").grid(column=1, row=1, sticky=W)
-		self.wth = Scale(self, from_=1, orient=HORIZONTAL, to=20,
-						resolution=1, length=200)
-		self.wth.grid(column=2, row=1, columnspan=2)
-		self.wth.set(wth)
+        Label(self, text="Time period resolution:").grid(column=1,
+                                                         row=2,
+                                                         sticky=W)
+        self.wtp = Scale(self, from_=1, orient=HORIZONTAL, to=1000,
+                         resolution=5, length=200)
+        self.wtp.grid(column=2, row=2, columnspan=2)
+        self.wtp.set(wtp)
 
+        Label(self, text="Default begin time:").grid(column=1,
+                                                     row=3,
+                                                     sticky=W)
+        self.wbt = Scale(self, from_=0, orient=HORIZONTAL, to=500,
+                         resolution=5, length=200)
+        self.wbt.grid(column=2, row=3, columnspan=2)
+        self.wbt.set(wbt)
 
-		Label(self, text="Time period resolution:").grid(column=1, row=2, sticky=W)
-		self.wtp = Scale(self, from_=1, orient=HORIZONTAL, to=1000,
-						resolution=5, length=200)
-		self.wtp.grid(column=2, row=2, columnspan=2)
-		self.wtp.set(wtp)
+        Label(self, text="Default ending time:").grid(column=1,
+                                                      row=4,
+                                                      sticky=W)
+        self.wet = Scale(self, from_=0, orient=HORIZONTAL, to=500,
+                         resolution=5, length=200)
+        self.wet.grid(column=2, row=4, columnspan=2)
+        self.wet.set(wet)
 
+        Button(self, text="Apply", width=15,
+               command=self.apply).grid(column=1, row=5, sticky=W)
+        Button(self, text="Quit", width=15,
+               command=self.window.destroy).grid(column=3, row=5, sticky=E)
 
-		Label(self, text="Default begin time:").grid(column=1, row=3, sticky=W)
-		self.wbt = Scale(self, from_=0, orient=HORIZONTAL, to=500,
-						resolution=5, length=200)
-		self.wbt.grid(column=2, row=3, columnspan=2)
-		self.wbt.set(wbt)
+        for child in self.winfo_children():
+            child.grid_configure(padx=5, pady=5)
 
-
-		Label(self, text="Default ending time:").grid(column=1, row=4, sticky=W)
-		self.wet = Scale(self, from_=0, orient=HORIZONTAL, to=500,
-						resolution=5, length=200)
-		self.wet.grid(column=2, row=4, columnspan=2)
-		self.wet.set(wet)
-
-		Button(self, text="Apply", width=15, command=self.apply).grid(column=1, row=5)
-		Button(self, text="Save config", width=15).grid(column=2, row=5)
-		Button(self, text="Quit", width=15, command=self.window.destroy).grid(column=3, row=5)
-
-		for child in self.winfo_children(): child.grid_configure(padx=5, pady=5)
-
-	def apply(self):
-		""" envoie les données """
-		self.rendu = (self.wth.get(), self.wtp.get(), self.wbt.get(), self.wet.get())
-		self.quit()
-		self.window.destroy()
-		return
-
-	def save(self):
-		""""""
-		pass
+    def apply(self):
+        """ button apply fonction """
+        self.rendu = (self.wth.get(), self.wtp.get(),
+                      self.wbt.get(), self.wet.get())
+        self.quit()
+        self.window.destroy()
+        return
 
 
 if __name__ == "__main__":
--- interface.py
+++ interface.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3.4
 # -*- coding: utf-8 -*-
 
+""" SDGI programm, require spike.py and configure.py """
+
 from tkinter import *
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
@@ -8,7 +10,7 @@ from tkinter.messagebox import showinfo, showwarning
 
 import matplotlib
 matplotlib.use('TkAgg')
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 
 from webbrowser import open as wbopen
@@ -23,7 +25,7 @@ class Interface(Frame):
         self.pack(fill=BOTH)
         self.window = window
 
-        #creation of variable 
+        # variable
         self.datafile = ""
         self.fig = None
         self.v = IntVar(self, 40)
@@ -32,13 +34,17 @@ class Interface(Frame):
         self.aex = IntVar(self, 150)
         self.information = StringVar(self, "None\n\nNone\n\nNone\n\nNone")
 
-        #creation de la barre de menu
+        # menu bar creation
         self.menubar = Menu(self)
 
         self.menu1 = Menu(self.menubar, tearoff=0)
-        self.menu1.add_command(label="File", accelerator="Ctrl + O", command=self.search_file)
+        self.menu1.add_command(label="File",
+                               accelerator="Ctrl + O",
+                               command=self.search_file)
         self.menu1.bind_all("<Control-o>", self.search_file)
-        self.menu1.add_command(label="Stream", accelerator="Ctrl + S", command=self.search_stream)
+        self.menu1.add_command(label="Stream",
+                               accelerator="Ctrl + S",
+                               command=self.search_stream)
         self.menu1.bind_all("<Control-s>", self.search_stream)
         self.menubar.add_cascade(label="Open", menu=self.menu1)
 
@@ -46,62 +52,98 @@ class Interface(Frame):
         self.menubar.add_command(label="Help", command=self.url_open)
         window.config(menu=self.menubar)
 
-        # création de la premiere frame (partie droite de l'interface)
+        # right main frame (for settings)
         self.frame1 = Frame(self, padx=10, pady=10)
         self.frame1.pack(side=RIGHT, fill=Y)
 
-        # création de la deuxième frame (partie gauche de l'interface)
+        # Left main frame (for drawing)
         self.drawingframe = Frame(self, width=8*100+10, height=5*100)
         self.drawingframe.pack(side=LEFT)
 
-        # sous frames contenues dans frame de droite 
-        self.frame1_3 = LabelFrame(self.frame1, text="Infos", width=26)
+        # Right frame children
+        self.frame1_3 = LabelFrame(self.frame1, text="Infos", width=27)
         self.frame1_3.pack()
 
         self.frame1_1 = LabelFrame(self.frame1, text="Settings")
         self.frame1_1.pack()
 
-        self.frame1_2 = Frame(self.frame1, width=50, height=50, padx=10, pady=10)
+        self.frame1_2 = Frame(self.frame1,
+                              width=50,
+                              height=50,
+                              padx=10,
+                              pady=10)
         self.frame1_2.pack(side=BOTTOM, fill=Y)
 
-
-        #frame 1 settings part
-        self.th = Scale(self.frame1_1, orient=HORIZONTAL, from_=20, to=80,
-                        resolution=10, tickinterval=5, variable=self.v,
-                        label="Threshold:", length=200)
+        # frame 1 settings part
+        self.th = Scale(self.frame1_1,
+                        orient=HORIZONTAL,
+                        from_=20,
+                        to=80,
+                        resolution=10,
+                        tickinterval=5,
+                        variable=self.v,
+                        label="Threshold:",
+                        length=200)
         self.th.grid(column=1, columnspan=2, row=1, sticky=(N, S, E, W))
 
-        self.timescale = Scale(self.frame1_1, orient=HORIZONTAL, from_=1000, to=10000,
-                               resolution=1000, label="Time Period:",
-                               length=200, variable=self.tp)
+        self.timescale = Scale(self.frame1_1,
+                               orient=HORIZONTAL,
+                               from_=1000,
+                               to=10000,
+                               resolution=1000,
+                               label="Time Period:",
+                               length=200,
+                               variable=self.tp)
         self.timescale.grid(column=1, columnspan=2, row=2, sticky=(N, S, E, W))
 
-        Label(self.frame1_1, text="Begin time:").grid(column=1, row=3, sticky=W)
-        self.axebegin = Spinbox(self.frame1_1, from_=0, to=500, textvariable=self.ax, width=6)
+        Label(self.frame1_1,
+              text="Begin time:").grid(column=1, row=3, sticky=W)
+        self.axebegin = Spinbox(self.frame1_1,
+                                from_=0,
+                                to=500,
+                                textvariable=self.ax,
+                                width=6)
         self.axebegin.grid(column=2, row=3, sticky=E)
 
         Label(self.frame1_1, text="End time:").grid(column=1, row=4, sticky=W)
-        self.axeend = Spinbox(self.frame1_1, from_=0,
-                              to=500, textvariable=self.aex, width=6)
+        self.axeend = Spinbox(self.frame1_1,
+                              from_=0,
+                              to=500,
+                              textvariable=self.aex,
+                              width=6)
         self.axeend.grid(column=2, row=4, sticky=E)
 
+        # frame 2 button part
+        Button(self.frame1_2,
+               text="Refresh",
+               command=self.print_graph,
+               width=15).pack()
+        Button(self.frame1_2,
+               text="Save",
+               command=self.save_fig,
+               width=15).pack()
+        Button(self.frame1_2,
+               text="Quit",
+               command=self.quit,
+               width=15).pack()
 
-        #frame 2 button part
-        Button(self.frame1_2, text="Refresh", command=self.print_graph, width=15).pack()
-        Button(self.frame1_2, text="Save", command=self.save_fig, width=15).pack()
-        Button(self.frame1_2, text="Quit", command=self.quit, width=15).pack()
-
-
-        #frame 3 infos part
-        Label(self.frame1_3, text="Current Threshold:\n\nCurrent Period time:\n\nData name:\n\nData length:",
-              justify="left", width=18).pack(side=LEFT)
-        self.info = Label(self.frame1_3, textvariable=self.information, justify="right", width=8)
+        # frame 3 infos part
+        Label(self.frame1_3,
+              text="Current Threshold:\n\nCurrent Period time:\n\n\
+Data name:\n\nData length:",
+              justify="left",
+              width=18).pack(side=LEFT)
+        self.info = Label(self.frame1_3,
+                          textvariable=self.information,
+                          justify="right",
+                          width=8)
         self.info.pack()
 
-
         # padding
-        for child in self.frame1_1.winfo_children(): child.grid_configure(padx=5, pady=5)
-        for child in self.frame1_2.winfo_children(): child.pack_configure(padx=5, pady=5)
+        for child in self.frame1_1.winfo_children():
+            child.grid_configure(padx=5, pady=5)
+        for child in self.frame1_2.winfo_children():
+            child.pack_configure(padx=5, pady=5)
 
     def print_graph(self):
         """
@@ -116,14 +158,21 @@ class Interface(Frame):
 
         if self.datafile != "":
             axe = (self.ax.get(), self.aex.get())
-            self.fig = draw_curb(self.datafile, self.v.get(), tp=self.tp.get(), axis=axe)
+            self.fig = draw_curb(self.datafile,
+                                 self.v.get(),
+                                 tp=self.tp.get(),
+                                 axis=axe)
 
             canvas = FigureCanvasTkAgg(self.fig, master=self.drawingframe)
             canvas.show()
             canvas.get_tk_widget().pack()
 
-            self.information.set("{} mV\n\n{} ms\n\n{}\n\n{} s".format(self.th.get(),
-                self.timescale.get(), self.datafile.split("/")[-1], self.axeend["to"]))
+            text = "{} mV\n\n{} \
+ms\n\n{}\n\n{} s".format(self.th.get(),
+                         self.timescale.get(),
+                         self.datafile.split("/")[-1],
+                         self.axeend["to"])
+            self.information.set(text)
         return
 
     def search_file(self, *args):
@@ -131,28 +180,41 @@ class Interface(Frame):
         allow to find file in
         your PATH
         """
-        self.datafile = askopenfilename(filetypes=[("text file", ".txt")], parent=self.window, title="Open a file")
+        self.datafile = askopenfilename(filetypes=[("text file", ".txt")],
+                                        parent=self.window,
+                                        title="Open a file")
         if self.datafile != "":
             try:
                 self.axeend["to"] = len(import_data(self.datafile)[1])/1000
                 self.axebegin["to"] = len(import_data(self.datafile)[1])/1000
                 self.print_graph()
             except ValueError:
-                showwarning(title="Warning !", message="Wrong file ...", parent=self.window)
+                showwarning(title="Warning !",
+                            message="Wrong file ...",
+                            parent=self.window)
         return
 
     def search_stream(self, *args):
         """ allow to find stream in your path """
-        showinfo(title="Info", message="Work in progress ...", parent=self.window)
+        showinfo(title="Info",
+                 message="Work in progress ...",
+                 parent=self.window)
+        return
 
     def save_fig(self):
         """ save the current plotting figure """
         try:
-            self.fig.savefig("{}_th={}_tp={}.png".format(self.datafile.split(".")[0],
-                self.th.get(), self.timescale.get()))
-            showinfo(title="Info", message="Plot save !", parent=self.window)
+            self.fig.savefig("{}_th={}_tp={}.png"
+                             .format(self.datafile.split(".")[0],
+                                     self.th.get(),
+                                     self.timescale.get()))
+            showinfo(title="Info",
+                     message="Plot save !",
+                     parent=self.window)
         except AttributeError:
-            showwarning(title="Error", message="No plot to save...", parent=self.window)
+            showwarning(title="Error",
+                        message="No plot to save...",
+                        parent=self.window)
         return
 
     def settings(self):
@@ -160,19 +222,24 @@ class Interface(Frame):
         wind = Toplevel(self.window)
         wind.geometry('+300+100')
         wind.title("Settings")
-        conf = Configure(wind, self.th["resolution"], self.timescale["resolution"],
-                        self.ax.get(), self.aex.get())
+        conf = Configure(wind,
+                         self.th["resolution"],
+                         self.timescale["resolution"],
+                         self.ax.get(),
+                         self.aex.get())
         wind.mainloop()
         if conf.rendu != ():
             self.th["resolution"] = conf.rendu[0]
-            self.timescale.configure(from_=conf.rendu[1], to=conf.rendu[1]*10, resolution=conf.rendu[1])
+            self.timescale.configure(from_=conf.rendu[1],
+                                     to=conf.rendu[1]*10,
+                                     resolution=conf.rendu[1])
             self.ax.set(conf.rendu[2])
             self.aex.set(conf.rendu[3])
         return
 
     def url_open(self):
         """ open a url """
-        return wbopen("http://genius.com/4936431")
+        return wbopen("https://github.com/Houet/Japon")
 
 
 if __name__ == "__main__":
